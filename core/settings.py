8a6edc9f95939727112ab1554b9e233437cea75b
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-05 09:15:08 +0100

    feat: enable API pagination

--- core/settings.py
+++ core/settings.py
@@ -126,6 +126,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
      'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
