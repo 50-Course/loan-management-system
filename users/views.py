94277c6314dcb4d4a3df9494b801864105cbc40a
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-05 09:07:48 +0100

    feat(users): implement token auth

--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import generics, serializers, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from users.models import Customer, LoanAdmin
+
+from .serializers import (AdminUserRegistrationSerializer,
+                          CustomerUserRegistrationSerializer,
+                          CustomTokenObtainPairSerializer)
+
+
+@extend_schema(tags=["Users"])
+class UserLoginView(TokenObtainPairView):
+    """
+    User login for token authentication.
+    """
+
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+@extend_schema(tags=["Users"])
+class UserRegisterView(generics.CreateAPIView):
+    """
+    Register new user (Customer or Admin).
+    """
+
+    def get_serializer_class(self):
+        role = self.request.data.get("role")
+
+        if role == "ADMIN":
+            return AdminUserRegistrationSerializer
+        elif role == "CUSTOMER":
+            return CustomerUserRegistrationSerializer
+        else:
+            raise serializers.ValidationError(
+                {"role": "Role must be either 'ADMIN' or 'CUSTOMER'."}
+            )
