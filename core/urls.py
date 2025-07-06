276b86aa312c0596fabad5d8bf42e1741fd0b9cc
50-Course <erioluwaadeodu@my.uopeople.edu>
2025-07-06 02:09:32 +0100

    docs(openapi): move openapi page to index root

--- core/urls.py
+++ core/urls.py
@@ -4,19 +4,19 @@ URL configuration for core project.
 
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import TokenRefreshView
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView,
                                    SpectacularSwaggerView)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("loans.urls")),
-    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/docs/",
+        "",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("", include("loans.urls")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("docs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("auth/", include("users.urls")),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
